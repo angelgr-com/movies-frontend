a865c466c96e9415578b3c7696b58888751b015d
angelgr-com <dev@angelgr.com>
2022-03-15 13:37:36 +0100

    style: basic banner and rowmovies styles

--- src/Components/RowMovies/RowMovies.jsx
+++ src/Components/RowMovies/RowMovies.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import styledComponents from 'styled-components';
+import Poster from '../Poster/Poster';
 
 const StyledRowMovies = styledComponents.div`
+  display: flex;
+  flex-direction: row;
   height: 15em;
   border: 0.1rem black solid;
   background-color: lightgray;
   width: 100%;
-  margin-bottom: 2em;
+  margin-bottom: 1em;
+  overflow: hidden;
 `;
 
 const RowMovies = () => {
   return (
-    <StyledRowMovies />
+    <StyledRowMovies>
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+    </StyledRowMovies>
   )
 };
 
