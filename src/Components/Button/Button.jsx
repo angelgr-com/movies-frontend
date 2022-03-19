f898a43e9e2bb971fd6a67e9e5571c0c6e384b08
angelgr-com <dev@angelgr.com>
2022-03-19 12:33:08 +0100

    feat: add home container with conditional rendering

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  margin: 0 1em;
   background-color: orange;
   border-radius: 0.5rem;
   border: 0;
   color: #fff;
   cursor: pointer;
   outline: 0;
-  padding: 1rem 2rem; 
+  padding: 0.5rem 1rem; 
 `;
 
 const Button = ({text, onClick}) => {
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,19 +1,40 @@
-import styledComponents from 'styled-components';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import Banner from '../../Components/Banner/Banner';
 import Row from '../../Components/Row/Row';
-
-const StyledHome = styledComponents.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  // background-color: hsl(331, 61%, 82%, 50%);
-  background-color: white;
-  width: 100%;
-  max-height: 100%;
-`;
+import Button from '../../Components/Button/Button';
+import styledComponents from 'styled-components';
 
 const Home = () => {
+  let navigate = useNavigate();
+
+  const toLogin = () => {
+    setTimeout(()=>{
+        navigate('/login');
+    },500)
+  }
+
+  const toRegister = () => {
+    setTimeout(()=>{
+        navigate('/register');
+    },500)
+  }
+
+  const toProfile = () => {
+    setTimeout(()=>{
+        navigate('/register');
+    },500)
+  }
+
+  const toOrders = () => {
+    setTimeout(()=>{
+        navigate('/orders');
+    },500)
+  }
+
+  // This hook checks if user is logged in and has a token
+  const [credentials, setCredentials] = useState(JSON.parse(localStorage.getItem("dataUser")));
+
   const genres = [
     { id: '28', genre: 'Action' },
     { id: '16', genre: 'Animation' },
@@ -37,7 +58,6 @@ const Home = () => {
   ];
 
   // const rows = [];
-
   // for (let i of genres) {
   //   rows.push(<Row genre={genres[i].genre} />);
   // }
@@ -46,32 +66,100 @@ const Home = () => {
   const max = 18, min = 0;
   const random = Math.floor(Math.random() * (max - min + 1) + min);
 
-
-  return (
-    <StyledHome>
-      <Banner random={random} id_genre={genres[random].id} />
-      {/* {rows} */}
-      <Row genre={genres[0].genre} id_genre={genres[0].id}/>
-      <Row genre={genres[1].genre} id_genre={genres[1].id}/>
-      <Row genre={genres[2].genre} id_genre={genres[2].id}/>
-      <Row genre={genres[3].genre} id_genre={genres[3].id}/>
-      <Row genre={genres[4].genre} id_genre={genres[4].id}/>
-      <Row genre={genres[5].genre} id_genre={genres[5].id}/>
-      <Row genre={genres[6].genre} id_genre={genres[6].id}/>
-      <Row genre={genres[7].genre} id_genre={genres[7].id}/>
-      <Row genre={genres[8].genre} id_genre={genres[8].id}/>
-      <Row genre={genres[9].genre} id_genre={genres[9].id}/>
-      <Row genre={genres[10].genre} id_genre={genres[10].id}/>
-      <Row genre={genres[11].genre} id_genre={genres[11].id}/>
-      <Row genre={genres[12].genre} id_genre={genres[12].id}/>
-      <Row genre={genres[13].genre} id_genre={genres[13].id}/>
-      <Row genre={genres[14].genre} id_genre={genres[14].id}/>
-      <Row genre={genres[15].genre} id_genre={genres[15].id}/>
-      <Row genre={genres[16].genre} id_genre={genres[16].id}/>
-      <Row genre={genres[17].genre} id_genre={genres[17].id}/>
-      <Row genre={genres[18].genre} id_genre={genres[18].id}/>
-    </StyledHome>
-  )
+  if (credentials?.dataUser?.token !== undefined) {
+    return (
+      <StyledHome>
+        <ButtonsRow>
+          <Button
+            text={credentials.dataUser.nombre} 
+            onClick={()=>toProfile()}
+          />
+          <Button
+            text='My Movies'
+            onClick={()=>toOrders()}
+          />
+        </ButtonsRow>
+        <Banner random={random} id_genre={genres[random].id} />
+        <Row genre={genres[0].genre} id_genre={genres[0].id} />
+        <Row genre={genres[1].genre} id_genre={genres[1].id} />
+        <Row genre={genres[2].genre} id_genre={genres[2].id} />
+        <Row genre={genres[3].genre} id_genre={genres[3].id} />
+        <Row genre={genres[4].genre} id_genre={genres[4].id} />
+        <Row genre={genres[5].genre} id_genre={genres[5].id} />
+        <Row genre={genres[6].genre} id_genre={genres[6].id} />
+        <Row genre={genres[7].genre} id_genre={genres[7].id} />
+        <Row genre={genres[8].genre} id_genre={genres[8].id} />
+        <Row genre={genres[9].genre} id_genre={genres[9].id} />
+        <Row genre={genres[10].genre} id_genre={genres[10].id} />
+        <Row genre={genres[11].genre} id_genre={genres[11].id} />
+        <Row genre={genres[12].genre} id_genre={genres[12].id} />
+        <Row genre={genres[13].genre} id_genre={genres[13].id} />
+        <Row genre={genres[14].genre} id_genre={genres[14].id} />
+        <Row genre={genres[15].genre} id_genre={genres[15].id} />
+        <Row genre={genres[16].genre} id_genre={genres[16].id} />
+        <Row genre={genres[17].genre} id_genre={genres[17].id} />
+        <Row genre={genres[18].genre} id_genre={genres[18].id} />
+      </StyledHome>
+    )
+  }
+  else {
+    return(
+      <StyledHome>
+        <ButtonsRow>
+          <Button
+            text='Login'
+            onClick={()=>toLogin()}
+          />
+          <Button
+            text='Register'
+            onClick={()=>toRegister()}
+          />
+        </ButtonsRow>
+        <Banner random={random} id_genre={genres[random].id} />
+        <Row genre={genres[0].genre} id_genre={genres[0].id}/>
+        <Row genre={genres[1].genre} id_genre={genres[1].id}/>
+        <Row genre={genres[2].genre} id_genre={genres[2].id}/>
+        <Row genre={genres[3].genre} id_genre={genres[3].id}/>
+        <Row genre={genres[4].genre} id_genre={genres[4].id}/>
+        <Row genre={genres[5].genre} id_genre={genres[5].id}/>
+        <Row genre={genres[6].genre} id_genre={genres[6].id}/>
+        <Row genre={genres[7].genre} id_genre={genres[7].id}/>
+        <Row genre={genres[8].genre} id_genre={genres[8].id}/>
+        <Row genre={genres[9].genre} id_genre={genres[9].id}/>
+        <Row genre={genres[10].genre} id_genre={genres[10].id}/>
+        <Row genre={genres[11].genre} id_genre={genres[11].id}/>
+        <Row genre={genres[12].genre} id_genre={genres[12].id}/>
+        <Row genre={genres[13].genre} id_genre={genres[13].id}/>
+        <Row genre={genres[14].genre} id_genre={genres[14].id}/>
+        <Row genre={genres[15].genre} id_genre={genres[15].id}/>
+        <Row genre={genres[16].genre} id_genre={genres[16].id}/>
+        <Row genre={genres[17].genre} id_genre={genres[17].id}/>
+        <Row genre={genres[18].genre} id_genre={genres[18].id}/>
+      </StyledHome>
+    )
+  }
 };
+
+const ButtonsRow = styledComponents.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  position: sticky;
+  top: 1em;
+  width: 100%;
+  z-index: 100;
+`;
+
+const StyledHome = styledComponents.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  // background-color: hsl(331, 61%, 82%, 50%);
+  background-color: white;
+  width: 100%;
+  max-height: 100%;
+`;
 
 export default Home;
