b4c1d930be5523b51eebd0c07efeb47c5cd0b240
angelgr-com <dev@angelgr.com>
2022-03-20 09:40:01 +0100

    feat: fetch data from redux store into MovieInfo

--- src/Containers/MovieInfo/MovieInfo.jsx
+++ src/Containers/MovieInfo/MovieInfo.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 
-const MovieDetail = () => {
+const MovieInfo = (props) => {
+  const posterURL = 'https://image.tmdb.org/t/p/w500' + props.search?.poster_path;
+
   return (
-    <div>MovieDetail</div>
+    <StyledInfo>
+      <Info>
+        <h1>{props.search?.title}</h1>
+        <span>Score: {props.search?.vote_average}/10</span><br/>
+        <span>Votes: {props.search?.vote_count}</span><br/>
+        <span>Release date: {props.search?.release_date}</span><br/>
+        <p>{props.search?.overview}</p>
+      </Info>
+      <Poster src={posterURL} alt=""/>
+    </StyledInfo>
   )
 };
 
-export default MovieDetail;
+const StyledInfo = styled.div`
+  background-color: #758787;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+`;
+
+const Info = styled.div`
+  width: 25%;
+  margin-right: 2em;
+  font-size: 1.5rem;
+`;
+
+const Poster = styled.img`
+  height: 25em;
+`;
+
+export default connect((state) => ({
+  credentials: state.credentials,
+  search : state.search.film
+}))(MovieInfo);
